7d4bee6c660527048a8cd5adb40559ca4ca57f4e
clrzlawrence <120644552+clrzlawrence@users.noreply.github.com>
2025-11-17 13:13:27 +0800

    test1

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Widget } from './widgets/widget.model';
+import { WidgetComponent } from './widgets/widgets';
+
+
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, WidgetComponent],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss']
 })
 export class Home {
+  cards: Widget[] = [
+    {
+      id: 1,
+      title: 'Pages',
+      number: 20,
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" /></svg>`
+    },
+    {
+      id: 2,
+      title: 'Posts',
+      number: 35,
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>`
+    },
+    {
+      id: 3,
+      title: 'Banners',
+      number: 2,
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg>'
+    },
+    {
+      id: 4,
+      title: 'Archives',
+      number: 44,
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" /></svg>'
+    },
 
+  ];
+
+  latestActivity = [
+  { text: 'Edited About Us page', user: 'Galvez', time: '2h ago', icon: 'assets/icons/edit.svg' },
+  { text: 'Created new post', user: 'James', time: '5h ago', icon: 'assets/icons/add.svg' },
+  { text: 'Deleted banner', user: 'Admin', time: '1d ago', icon: 'assets/icons/delete.svg' },
+];
 }
+
+
